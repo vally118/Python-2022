51b98d0f0bf7f81f9f80610f8b31f6709bfa0231
Valentin Ionita <ionita.valentin86@gmail.com>
2022-07-03 19:28:14 +0300

    Doar un mesaj pentru update

--- hello.py
+++ hello.py
@@ -1,14 +1,1 @@
-import os
-os.system('clear')
-
-
-
-o_lista = ['Baleni', 'Vali', 4]
-
-fav_pizza = {
-	"John":"Peperonni",
-	"Valli":"cu de toate",
-	"Mary":"simpla"
-}
-
-print (fav_pizza["John"])
+# Learn Python again in 2022
