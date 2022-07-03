24de5e31af838c4256ad81e181f318d9c2c439e6
Valentin Ionita <ionita.valentin86@gmail.com>
2022-07-03 19:14:28 +0300

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+import os
+os.system('clear')
+
+
+
+o_lista = ['Baleni', 'Vali', 4]
+
+fav_pizza = {
+	"John":"Peperonni",
+	"Valli":"cu de toate",
+	"Mary":"simpla"
+}
+
+print (fav_pizza["John"])
